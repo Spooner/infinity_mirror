3afd960860805e02e431cef29358a98ca1edde75
spooner <bil.bagpuss@gmail.com>
2017-12-17 01:06:05 +0000

    added extra modes, inner box and bottom panel cover

--- Sketchbook/infinity_mirror/infinity_mirror.ino
+++ Sketchbook/infinity_mirror/infinity_mirror.ino
@@ -3,13 +3,14 @@
 #define NUM_LEDS 24
 
 #define MODE_OFF 0
-#define MODE_ON 1
-#define MODE_ROTATE_SLOW 2
+#define MODE_ALL_ON 1
+#define MODE_CLAP 2
 #define MODE_ROTATE_FAST 3
-#define NUM_MODES 4
+#define MODE_RAINBOW 4
+#define MODE_XMAS 5
+#define NUM_MODES 6
 
-#define SLOW_REPEAT 4
-#define FAST_REPEAT 6
+#define FAST_REPEAT 8
 
 const int BUTTON_PIN = 2;
 const int LED_DATA_PIN = 6;
@@ -17,7 +18,7 @@ const int BRIGHTNESS = 16;
 
 int last_button_state = LOW;
 int button_state;
-int mode = MODE_ON;
+int mode = MODE_ALL_ON;
 float rotate_offset = 0;
 unsigned long last_debounce_time = 0;
 unsigned long debounce_delay = 50;
@@ -38,6 +39,12 @@ void setup() {
 }
 
 void loop() {
+  check_button();
+  draw();
+  delay(5);
+}
+
+void check_button() {
   int reading = digitalRead(BUTTON_PIN);
 
   if (reading != last_button_state) {
@@ -61,7 +68,10 @@ void loop() {
     }
   }
   last_button_state = reading;
-  
+}
+
+void draw() {
+  int offset;
   FastLED.clear();
   
   switch (mode) {
@@ -69,28 +79,27 @@ void loop() {
       Serial.println("Mode: Off");
       break;
       
-    case MODE_ON:
-      Serial.println("Mode: On");
+    case MODE_ALL_ON:
+      Serial.println("Mode: All On");
       for (int i = 0; i < NUM_LEDS; i++) {
         leds[i] = LIGHTER;
       }
       break;
       
-    case MODE_ROTATE_SLOW:
-      Serial.println("Mode: Rotate slow");
-      rotate_offset += 0.05;
-      for (int i = int(rotate_offset) % SLOW_REPEAT; i < NUM_LEDS; i += SLOW_REPEAT) {
-        leds[i] = LIGHTER;
+    case MODE_CLAP:
+      Serial.println("Mode: Clap");
+      rotate_offset += 0.1;
+      offset = int(rotate_offset) % 12;
+      if (offset >= 6) {
+        offset = 11 - offset;
       }
-      for (int i = int(rotate_offset - 1) % SLOW_REPEAT; i < NUM_LEDS; i += SLOW_REPEAT) {
-        leds[i] = DARKER;
-      }
-      break;
+      leds[offset] = leds[11 - offset] = leds[12 + offset] = leds[23 - offset] = LIGHTER;
+      break;     
       
     case MODE_ROTATE_FAST:
       Serial.println("Mode: Rotate fast");
       rotate_offset += 0.2;
-      for (int i = int(rotate_offset) % FAST_REPEAT; i < NUM_LEDS; i + =FAST_REPEAT) {
+      for (int i = int(rotate_offset) % FAST_REPEAT; i < NUM_LEDS; i += FAST_REPEAT) {
         leds[i] = LIGHTER;
       }
       for (int i = int(rotate_offset - 1) % FAST_REPEAT; i < NUM_LEDS; i += FAST_REPEAT) {
@@ -103,9 +112,29 @@ void loop() {
         leds[i] = DARKEST;
       }
       break;
-  }
+      
+    case MODE_RAINBOW:
+      Serial.println("Mode: Rainbow");
+      rotate_offset += 0.5;
+      for (int i = 0; i < NUM_LEDS; i++) {
+        leds[i].setHSV(int(rotate_offset) % 256, 200, 255);
+      }
+      break;
+      
+    case MODE_XMAS:
+      Serial.println("Mode: XMas");
+      rotate_offset += 0.01;
+      if (int(rotate_offset) % 2 == 0) {
+        for (int i = 0; i < NUM_LEDS; i += 2) {
+          leds[i] = CRGB::Red;
+        }
+      } else {
+        for (int i = 1; i < NUM_LEDS; i += 2) {
+          leds[i] = CRGB::Green;
+        }
+      }
+      break;
+  }   
   
   FastLED.show();
-  
-  delay(5);
 }
